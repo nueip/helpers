7a01463ee8af125c8f273c055f2a12e12ad4b612
mars.hung <mars.hung@staff.nueip.com>
2018-07-19 15:52:51 +0800

    [Patch] 智慧判斷函式庫修正-使用EncodeHelper::snapshotEncode,snapshotDecode

--- EncodeHelper.php
+++ EncodeHelper.php
@@ -7,8 +7,8 @@ namespace app\helpers;
  * @author Mars Hung <tfaredxj@gmail.com>
  * @example <br>
  *          1. Snapshot encode(zip) & decode(unzip) <br>
- *          - encode: $snapshot = app\helpers\EncodeHelper::snapshotEncode($data); <br>
- *          - decode: $data = app\helpers\EncodeHelper::snapshotDecode($snapshot); <br>
+ *          - encode: $snapshot = \app\helpers\EncodeHelper::snapshotEncode($data); <br>
+ *          - decode: $data = \app\helpers\EncodeHelper::snapshotDecode($snapshot); <br>
  *         
  */
 class EncodeHelper
