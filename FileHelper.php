053ab46edf2d6fb25b4f12fd27dccb822cc1cf53
jack.lin <jack.lin@staff.nueip.com>
2018-10-15 17:51:37 +0800

    Merge branch 'master' of https://gitlab.com/nueip/cloud.nueip.com into jack.lin_cardno-duplicate-fix

--- FileHelper.php
+++ FileHelper.php
@@ -21,7 +21,17 @@ class FileHelper
         if (!$_FILES) {
             return false;
         }
-
-        // ...
+        // initialize the size counter
+        $totalSize = 0;
+        // Calculate the size of all uploaded files
+        foreach ($_FILES['files']['size'] as $key => $kbs) {
+            $totalSize += $kbs;
+        }
+        // whether size exceeds the limitation or not
+        if (($totalSize / 1024) > $maxKB) {
+            return false;
+        }
+        
+        return true;
     }
 }
