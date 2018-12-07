aca92df043aae3c1112eb84d83dce0aab2ca0bea
nick.lai <nick.lai@staff.nueip.com>
2018-12-07 15:02:32 +0800

    Resolve file name conflict `DatetimeHelper.php`

--- DateTimeHelper.php
+++ DateTimeHelper.php
@@ -10,6 +10,38 @@ namespace app\helpers;
 class DateTimeHelper
 {
     /**
+     * Get dates between $start and $end
+     *
+     * @param string $start
+     * @param string $end
+     * @param string $format
+     *
+     * @example
+     *  $start = '2010-01-30';
+     *  $end = '2010-02-01';
+     *  $format = "Y-m-d";
+     *  $dates = \app\helpers\DateTimeHelper::getDates($start, $end, $format);
+     *  // Result: $dates -> (array) ['2010-01-30', '2010-01-31', '2010-02-01'];
+     *
+     * @return array $dates
+     */
+    public static function getDates($start, $end, $format = "Y-m-d")
+    {
+        $start = new \DateTime($start);
+        $end = new \DateTime($end);
+        $interval = \DateInterval::createFromDateString('1 day');
+        $period = new \DatePeriod($start, $interval, $end->modify('+1 day'));
+
+        $dates = [];
+
+        foreach ($period as $dt) {
+            $dates[] = $dt->format($format);
+        }
+
+        return $dates;
+    }
+
+    /**
      * Shift a giving months from your input datetime with month based
      * 
      * If the target month is smaller than the original month, the extra days will be discarded.
