28603540faf7e4b5c9313becb237f532c27eeffa
Gunter <gunter.chou@staff.nueip.com>
2019-01-15 17:12:23 +0800

    [Patch] FileHelper 新增 filterFilename 檔名過濾

--- FileHelper.php
+++ FileHelper.php
@@ -34,4 +34,34 @@ class FileHelper
         
         return true;
     }
+
+    /**
+     * Filter filename
+     * 
+     * @param string $filename
+     * @return string
+     */
+    public static function filterFilename($filename)
+    {
+        // 保留字元
+        static $reservedChar = [
+            '<', '>', '|', ':', '&',
+            '*', '(', ')', ';' ,'"',
+            '/', '?', '\\', '\'',
+        ];
+
+        // 移除保留字元
+        $filename = str_replace($reservedChar, '', $filename);
+
+        // 空白符號 轉換成 底線
+        $filename = str_replace(' ', '_', $filename);
+
+        // 檔名長度小於一時報錯
+        if (is_null($filename[0])) {
+            $errorMsg = get_language('filedes') . ':' . get_language('wrongformat_02');
+            throw new Exception($errorMsg, 400);
+        }
+
+        return $filename;
+    }
 }
